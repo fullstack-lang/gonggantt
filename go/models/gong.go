198002c72cbe1c10f2121abaeb76b21a4d8d653c
peugeot <thomaspeugeot@yahoo.fr>
2022-05-07 12:35:13 +0200

    new gongc

--- go/models/gong.go
+++ go/models/gong.go
@@ -280,8 +280,8 @@ func (arrow *Arrow) GetName() (res string) {
 }
 
 func (arrow *Arrow) GetFields() (res []string) {
-	// list of fields 
-	res = []string{"Name", "From", "To", "OptionnalColor", "OptionnalStroke",  }
+	// list of fields
+	res = []string{"Name", "From", "To", "OptionnalColor", "OptionnalStroke"}
 	return
 }
 
@@ -414,8 +414,8 @@ func (bar *Bar) GetName() (res string) {
 }
 
 func (bar *Bar) GetFields() (res []string) {
-	// list of fields 
-	res = []string{"Name", "Start", "End", "OptionnalColor", "OptionnalStroke", "FillOpacity", "StrokeWidth", "StrokeDashArray",  }
+	// list of fields
+	res = []string{"Name", "Start", "End", "OptionnalColor", "OptionnalStroke", "FillOpacity", "StrokeWidth", "StrokeDashArray"}
 	return
 }
 
@@ -550,8 +550,8 @@ func (gantt *Gantt) GetName() (res string) {
 }
 
 func (gantt *Gantt) GetFields() (res []string) {
-	// list of fields 
-	res = []string{"Name", "ComputedStart", "ComputedEnd", "UseManualStartAndEndDates", "ManualStart", "ManualEnd", "LaneHeight", "RatioBarToLaneHeight", "YTopMargin", "XLeftText", "TextHeight", "XLeftLanes", "XRightMargin", "ArrowLengthToTheRightOfStartBar", "ArrowTipLenght", "TimeLine_Color", "TimeLine_FillOpacity", "TimeLine_Stroke", "TimeLine_StrokeWidth", "Group_Stroke", "Group_StrokeWidth", "Group_StrokeDashArray", "DateYOffset", "AlignOnStartEndOnYearStart", "Lanes", "Milestones", "Groups", "Arrows",  }
+	// list of fields
+	res = []string{"Name", "ComputedStart", "ComputedEnd", "UseManualStartAndEndDates", "ManualStart", "ManualEnd", "LaneHeight", "RatioBarToLaneHeight", "YTopMargin", "XLeftText", "TextHeight", "XLeftLanes", "XRightMargin", "ArrowLengthToTheRightOfStartBar", "ArrowTipLenght", "TimeLine_Color", "TimeLine_FillOpacity", "TimeLine_Stroke", "TimeLine_StrokeWidth", "Group_Stroke", "Group_StrokeWidth", "Group_StrokeDashArray", "DateYOffset", "AlignOnStartEndOnYearStart", "Lanes", "Milestones", "Groups", "Arrows"}
 	return
 }
 
@@ -746,8 +746,8 @@ func (group *Group) GetName() (res string) {
 }
 
 func (group *Group) GetFields() (res []string) {
-	// list of fields 
-	res = []string{"Name", "GroupLanes",  }
+	// list of fields
+	res = []string{"Name", "GroupLanes"}
 	return
 }
 
@@ -875,8 +875,8 @@ func (lane *Lane) GetName() (res string) {
 }
 
 func (lane *Lane) GetFields() (res []string) {
-	// list of fields 
-	res = []string{"Name", "Order", "Bars",  }
+	// list of fields
+	res = []string{"Name", "Order", "Bars"}
 	return
 }
 
@@ -1006,8 +1006,8 @@ func (laneuse *LaneUse) GetName() (res string) {
 }
 
 func (laneuse *LaneUse) GetFields() (res []string) {
-	// list of fields 
-	res = []string{"Name", "Lane",  }
+	// list of fields
+	res = []string{"Name", "Lane"}
 	return
 }
 
@@ -1132,8 +1132,8 @@ func (milestone *Milestone) GetName() (res string) {
 }
 
 func (milestone *Milestone) GetFields() (res []string) {
-	// list of fields 
-	res = []string{"Name", "Date", "DisplayVerticalBar", "LanesToDisplayMilestoneUse",  }
+	// list of fields
+	res = []string{"Name", "Date", "DisplayVerticalBar", "LanesToDisplayMilestoneUse"}
 	return
 }
 
@@ -1907,5 +1907,161 @@ func generatesIdentifier(gongStructName string, idx int, instanceName string) (i
 
 	return
 }
+
+// insertion point of functions that provide maps for reverse associations
+// generate function for reverse association maps of Arrow
+func (stageStruct *StageStruct) CreateReverseMap_Arrow_From() (res map[*Bar][]*Arrow) {
+	res = make(map[*Bar][]*Arrow)
+
+	for arrow := range stageStruct.Arrows {
+		if arrow.From != nil {
+			bar_ := arrow.From
+			var arrows []*Arrow
+			_, ok := res[bar_]
+			if ok {
+				arrows = res[bar_]
+			} else {
+				arrows = make([]*Arrow, 0)
+			}
+			arrows = append(arrows, arrow)
+			res[bar_] = arrows
+		}
+	}
+
+	return
+}
+
+func (stageStruct *StageStruct) CreateReverseMap_Arrow_To() (res map[*Bar][]*Arrow) {
+	res = make(map[*Bar][]*Arrow)
+
+	for arrow := range stageStruct.Arrows {
+		if arrow.To != nil {
+			bar_ := arrow.To
+			var arrows []*Arrow
+			_, ok := res[bar_]
+			if ok {
+				arrows = res[bar_]
+			} else {
+				arrows = make([]*Arrow, 0)
+			}
+			arrows = append(arrows, arrow)
+			res[bar_] = arrows
+		}
+	}
+
+	return
+}
+
+// generate function for reverse association maps of Bar
+// generate function for reverse association maps of Gantt
+func (stageStruct *StageStruct) CreateReverseMap_Gantt_Lanes() (res map[*Lane]*Gantt) {
+	res = make(map[*Lane]*Gantt)
+
+	for gantt := range stageStruct.Gantts {
+		for _, lane_ := range gantt.Lanes {
+			res[lane_] = gantt
+		}
+	}
+
+	return
+}
+
+func (stageStruct *StageStruct) CreateReverseMap_Gantt_Milestones() (res map[*Milestone]*Gantt) {
+	res = make(map[*Milestone]*Gantt)
+
+	for gantt := range stageStruct.Gantts {
+		for _, milestone_ := range gantt.Milestones {
+			res[milestone_] = gantt
+		}
+	}
+
+	return
+}
+
+func (stageStruct *StageStruct) CreateReverseMap_Gantt_Groups() (res map[*Group]*Gantt) {
+	res = make(map[*Group]*Gantt)
+
+	for gantt := range stageStruct.Gantts {
+		for _, group_ := range gantt.Groups {
+			res[group_] = gantt
+		}
+	}
+
+	return
+}
+
+func (stageStruct *StageStruct) CreateReverseMap_Gantt_Arrows() (res map[*Arrow]*Gantt) {
+	res = make(map[*Arrow]*Gantt)
+
+	for gantt := range stageStruct.Gantts {
+		for _, arrow_ := range gantt.Arrows {
+			res[arrow_] = gantt
+		}
+	}
+
+	return
+}
+
+// generate function for reverse association maps of Group
+func (stageStruct *StageStruct) CreateReverseMap_Group_GroupLanes() (res map[*Lane]*Group) {
+	res = make(map[*Lane]*Group)
+
+	for group := range stageStruct.Groups {
+		for _, lane_ := range group.GroupLanes {
+			res[lane_] = group
+		}
+	}
+
+	return
+}
+
+// generate function for reverse association maps of Lane
+func (stageStruct *StageStruct) CreateReverseMap_Lane_Bars() (res map[*Bar]*Lane) {
+	res = make(map[*Bar]*Lane)
+
+	for lane := range stageStruct.Lanes {
+		for _, bar_ := range lane.Bars {
+			res[bar_] = lane
+		}
+	}
+
+	return
+}
+
+// generate function for reverse association maps of LaneUse
+func (stageStruct *StageStruct) CreateReverseMap_LaneUse_Lane() (res map[*Lane][]*LaneUse) {
+	res = make(map[*Lane][]*LaneUse)
+
+	for laneuse := range stageStruct.LaneUses {
+		if laneuse.Lane != nil {
+			lane_ := laneuse.Lane
+			var laneuses []*LaneUse
+			_, ok := res[lane_]
+			if ok {
+				laneuses = res[lane_]
+			} else {
+				laneuses = make([]*LaneUse, 0)
+			}
+			laneuses = append(laneuses, laneuse)
+			res[lane_] = laneuses
+		}
+	}
+
+	return
+}
+
+// generate function for reverse association maps of Milestone
+func (stageStruct *StageStruct) CreateReverseMap_Milestone_LanesToDisplayMilestoneUse() (res map[*LaneUse]*Milestone) {
+	res = make(map[*LaneUse]*Milestone)
+
+	for milestone := range stageStruct.Milestones {
+		for _, laneuse_ := range milestone.LanesToDisplayMilestoneUse {
+			res[laneuse_] = milestone
+		}
+	}
+
+	return
+}
+
 
 // insertion point of enum utility functions
