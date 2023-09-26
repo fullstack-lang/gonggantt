4609099ef9d1ce4abc0c7e1da6c845b5cd1c60a2
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-09-26 18:34:19 +0200

    simplification

--- ng/projects/gonggantt/src/public-api.ts
+++ ng/projects/gonggantt/src/public-api.ts
@@ -11,26 +11,5 @@ export * from './lib/commitnbfromback.service'
 export * from './lib/push_from_front_nb.service'
 
 
-export * from './lib/arrow-db'
-export * from './lib/arrow.service'
-
-export * from './lib/bar-db'
-export * from './lib/bar.service'
-
-export * from './lib/gantt-db'
-export * from './lib/gantt.service'
-
-export * from './lib/group-db'
-export * from './lib/group.service'
-
-export * from './lib/lane-db'
-export * from './lib/lane.service'
-
-export * from './lib/laneuse-db'
-export * from './lib/laneuse.service'
-
-export * from './lib/milestone-db'
-export * from './lib/milestone.service'
-
 
 export * from './lib/GanttStacksNames'
--- ng/projects/gongganttdatamodel/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of gongganttdatamodel
- */
-
-export * from './lib/gongganttdatamodel.service';
-export * from './lib/gongganttdatamodel.component';
-export * from './lib/gongganttdatamodel.module';
