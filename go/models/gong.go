247478e3d10a168a96e0fc361d1e41ffaf6e5cf1
peugeot <thomaspeugeot@yahoo.fr>
2023-09-30 17:35:32 +0200

    new gongc

--- go/models/gong.go
+++ go/models/gong.go
@@ -4,7 +4,6 @@ package models
 import (
 	"errors"
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -171,17 +170,6 @@ type BackRepoInterface interface {
 	CheckoutMilestone(milestone *Milestone)
 	GetLastCommitFromBackNb() uint
 	GetLastPushFromFrontNb() uint
-}
-
-var _stage *StageStruct
-
-var once sync.Once
-
-func GetDefaultStage() *StageStruct {
-	once.Do(func() {
-		_stage = NewStage("")
-	})
-	return _stage
 }
 
 func NewStage(path string) (stage *StageStruct) {
